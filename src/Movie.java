819a67c8c3617060480d8da4723e1f26e0ad1188
JackNel <jcneligan@gmail.com>
2015-10-14 14:40:25 -0400

    Added methods: create title, create director, create country, create year, create minutes, save

--- src/Movie.java
+++ src/Movie.java
@@ -23,9 +23,60 @@ public class Movie {
     }
 
     void chooseTitle() {
-        System.out.println("What is the title of the movie you wish to catalog?");
+        System.out.println("What is the title of the movie you wish to add to catalog?");
         Scanner scanner = new Scanner(System.in);
-        String title = scanner.nextLine();
+        String newTitle = scanner.nextLine();
+        title = newTitle;
     }
 
+    void chooseDirector() {
+        System.out.println(String.format("OK, the title of the movie is %s. Next, enter the name of the director of %s.", title, title));
+        Scanner scanner = new Scanner(System.in);
+        String newDirector = scanner.nextLine();
+        director = newDirector;
+    }
+
+    void chooseCountry() {
+        System.out.println(String.format("So, %s was directed by %s.  What is the country of origin?", title, director));
+        Scanner scanner = new Scanner(System.in);
+        String newCountry = scanner.nextLine();
+        country = newCountry;
+    }
+
+    void chooseYear() {
+        System.out.println(String.format("%s was directed by %s and was originally released in %s.  What year was %s made?", title, director, country, title));
+        Scanner scanner = new Scanner(System.in);
+        String newYear = scanner.nextLine();
+        int newYearNum = Integer.valueOf(newYear);
+        year = newYearNum;
+    }
+
+    void chooseMinutes() {
+        System.out.println(String.format("%s, directed by %s, was released in %s in the year %d.  How long (in minutes) is %s?", title, director, country, year, title));
+        Scanner scanner = new Scanner(System.in);
+        String newMin = scanner.nextLine();
+        int newMinNum = Integer.valueOf(newMin);
+        minutes = newMinNum;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
 }//class Movie
--- src/Movies.java
+++ src/Movies.java
@@ -1,3 +1,8 @@
+import jodd.json.JsonSerializer;
+
+import java.io.File;
+import java.io.FileWriter;
+
 /**
  * Created by Jack on 10/14/15.
  */
@@ -9,5 +14,29 @@ public class Movies {
         System.out.println("Let's begin!");
         movie = new Movie();
         movie.chooseTitle();
+        movie.chooseDirector();
+        movie.chooseCountry();
+        movie.chooseYear();
+        movie.chooseMinutes();
+
+        saveGame();
+
+
+    }//main method
+
+    static void saveGame() {
+        File f = new File("save.json");
+        JsonSerializer serializer = new JsonSerializer();
+        String contentToSave = serializer.serialize(movie);
+
+        try {
+            FileWriter fw = new FileWriter(f);
+            fw.write(contentToSave);
+            fw.close();
+        } catch (Exception e) {
+
+        }
     }
-}
+}//class Movies
+
+
